799378c26d9af40e1ea15b53548862b2f7d6f0cb
Brahm Van Houzen <brahmvanh@gmail.com>
2023-11-09 12:19:39 -0500

    Got the motion path I was looking for. Need to work on the duration and ease

--- GSAP/animation_demos/src/BouncingSvg/index.js
+++ GSAP/animation_demos/src/BouncingSvg/index.js
@@ -1,22 +1,44 @@
 import React, { useLayoutEffect, useRef } from 'react';
 import gsap from 'gsap';
+import { MotionPathPlugin } from 'gsap/all';
 
 import './style.css';
 
 function BouncingSvg() {
+	gsap.registerPlugin(MotionPathPlugin);
 	var ball = document.querySelectorAll('.ball');
 	console.log(ball);
 	const main = useRef();
-  const svgRef = useRef();
+	const svgRef = useRef();
+
+	const getSemicircleCoordinate = (theta) => {
+		const radius = 200;
+		const x = 200 + radius * Math.cos(theta);
+		const y = -200 + radius * Math.sin(theta);
+		return [x, y];
+	};
+
+	// Create a function to convert a point on a semicircle to a coordinate pair.
+	const createMotionPath = (theta) => {
+		const semicircleCoordinate = getSemicircleCoordinate(theta);
+		// Sample the semicircle at 100 points.
+		const samples = gsap.utils.toArray(Array(100).fill(0));
+		const coordinates = samples.map(semicircleCoordinate);
+
+		// Return a MotionPath plugin instance.
+		return new MotionPathPlugin({
+			path: coordinates,
+		});
+	};
 
 	useLayoutEffect(() => {
 		let ctx = gsap.context(() => {
 			var tl = gsap.timeline({ repeat: -1, yoyo: false });
 			console.log(tl);
-      // tl.set('#svg', { y: 600});
+			// tl.set('#svg', { y: 600});
 			tl.to('#svg', {
 				y: 100,
-        x: 0,
+				x: 0,
 				ease: 'sine.inOut',
 				duration: 0.5,
 			});
@@ -33,18 +55,18 @@ function BouncingSvg() {
 				},
 				'-=.05'
 			);
-      tl.to('#svg', {
-        y: -200,
-        x: 200,
-        ease: 'sine.inOut',
-        duration: 1
-      })
-      tl.to('#svg', {
-        y: -150,
-        x: 300,
-        ease: 'sine.inOut',
-        duration: 1
-      })
+			tl.to('#svg', {
+				scaleY: 1,
+				duration: 0
+			});
+			tl.to('#svg', {
+				motionPath: [
+					{ x: 0, y: 100 },
+					{ x: 200, y: -400 },
+					{ x: 350, y: -200 },
+				],
+				duration: 3,
+			});
 		}, main);
 
 		return () => ctx.revert();
