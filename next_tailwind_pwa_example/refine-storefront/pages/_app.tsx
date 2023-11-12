fd8df915516b8d1274222397643c218d1177e70d
Brahm Van Houzen <brahmvanh@gmail.com>
2023-11-11 23:20:58 -0500

    Worked a door shift, drank some beer, wrote some code. great night. the architecture of this demo app feels weird. Next is cool tho.

--- next_tailwind_pwa_example/refine-storefront/pages/_app.tsx
+++ next_tailwind_pwa_example/refine-storefront/pages/_app.tsx
@@ -1,64 +1,61 @@
-import { GitHubBanner, Refine } from "@refinedev/core";
-import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-import routerProvider, {
-  DocumentTitleHandler,
-  UnsavedChangesNotifier,
-} from "@refinedev/nextjs-router";
-import type { NextPage } from "next";
-import { AppProps } from "next/app";
+import { GitHubBanner, Refine } from '@refinedev/core';
+import { DevtoolsPanel, DevtoolsProvider } from '@refinedev/devtools';
+import { RefineKbar, RefineKbarProvider } from '@refinedev/kbar';
+import routerProvider, { DocumentTitleHandler, UnsavedChangesNotifier } from '@refinedev/nextjs-router';
+import type { NextPage } from 'next';
+import { AppProps } from 'next/app';
 
-import { Layout } from "@components/layout";
-import dataProvider from "@refinedev/simple-rest";
-import "@styles/global.css";
+import { Layout } from '@components/Layout';
+import dataProvider from '@refinedev/simple-rest';
+import '@styles/global.css';
 
-const API_URL = "https://api.fake-rest.refine.dev";
+const API_URL = 'https://fakestoreapi.com';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  noLayout?: boolean;
+	noLayout?: boolean;
 };
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
+	Component: NextPageWithLayout;
 };
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout): JSX.Element {
-  const renderComponent = () => {
-    if (Component.noLayout) {
-      return <Component {...pageProps} />;
-    }
+	const renderComponent = () => {
+		if (Component.noLayout) {
+			return <Component {...pageProps} />;
+		}
 
-    return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    );
-  };
+		return (
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		);
+	};
 
-  return (
-    <>
-      <GitHubBanner />
-      <RefineKbarProvider>
-        <DevtoolsProvider>
-          <Refine
-            routerProvider={routerProvider}
-            dataProvider={dataProvider(API_URL)}
-            options={{
-              syncWithLocation: true,
-              warnWhenUnsavedChanges: true,
-              projectId: "CzVBDy-inFhZH-IH8MX9",
-            }}
-          >
-            {renderComponent()}
-            <RefineKbar />
-            <UnsavedChangesNotifier />
-            <DocumentTitleHandler />
-          </Refine>
-          <DevtoolsPanel />
-        </DevtoolsProvider>
-      </RefineKbarProvider>
-    </>
-  );
+	return (
+		<>
+			<GitHubBanner />
+			<RefineKbarProvider>
+				<DevtoolsProvider>
+					<Refine
+						resources={[{ name: 'products', list: '/' }]}
+						routerProvider={routerProvider}
+						dataProvider={dataProvider(API_URL)}
+						options={{
+							syncWithLocation: true,
+							warnWhenUnsavedChanges: true,
+							projectId: 'CzVBDy-inFhZH-IH8MX9',
+						}}>
+						{renderComponent()}
+						<RefineKbar />
+						<UnsavedChangesNotifier />
+						<DocumentTitleHandler />
+					</Refine>
+					<DevtoolsPanel />
+				</DevtoolsProvider>
+			</RefineKbarProvider>
+		</>
+	);
 }
 
 export default MyApp;
--- next_tailwind_pwa_example/refine-storefront/pages/index.tsx
+++ next_tailwind_pwa_example/refine-storefront/pages/index.tsx
@@ -1,7 +1,44 @@
-import { WelcomePage } from "@refinedev/core";
+import { GetServerSideProps } from 'next';
+import dataProvider from '@refinedev/simple-rest';
+import { GetListResponse, useTable } from '@refinedev/core';
+import ProductCards from '@components/ProductCards';
 
-export default function Index() {
-  return <WelcomePage />;
+interface IProduct {
+	id: number;
+	title: string;
+	price: number;
+	description: string;
+	category: string;
+	image: string;
 }
+type ItemProp = {
+	products: GetListResponse<IProduct>;
+};
 
-Index.noLayout = true;
+const ProductList: React.FC<ItemProp> = ({ products }) => {
+	const { tableQueryResult } = useTable<IProduct>({
+		resource: 'products',
+		queryOptions: {
+			initialData: products,
+		},
+	});
+
+	return (
+		<div className='grid grid-cols-4 gap-6 px-24 my-8'>
+			{tableQueryResult.data?.data.map((product) => {
+				return <ProductCards key={product.id} title={product.title} category={product.category} description={product.description} cardImage={product.image} price={product.price} />;
+			})}
+		</div>
+	);
+};
+
+export default ProductList;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const data = await dataProvider('https://fakestoreapi.com').getList<IProduct>({
+		resource: 'products',
+	});
+	return {
+		props: { products: data },
+	};
+};
